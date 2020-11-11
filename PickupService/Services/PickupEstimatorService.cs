3eddf8f3970dc5e0cf58199af769c70437d22f69
Brian Dyko <32283478+briandyko@users.noreply.github.com>
2020-11-11 13:45:34 -0500

    Add project files.

---
+++ PickupService/Services/PickupEstimatorService.cs
@@ -0,0 +1,31 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PickupService.Services
+{
+    public class PickupEstimatorService : PickupEstimator.PickupEstimatorBase
+    {
+        public override Task<PickupResponse> GetPickupTime(PickupRequest request, ServerCallContext context)
+        {
+            //for orders under 5 items, tomorrow, over five items, two days.
+            int days = 0;
+            if (request.Items.Count <= 5)
+            {
+                days = 1;
+            }
+            else
+            {
+                days = 2;
+            }
+            var response = new PickupResponse
+            {
+                PickupTime = Timestamp.FromDateTime(DateTime.Now.AddDays(days).ToUniversalTime())
+            };
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ TestClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using PickupService;
+using System;
+
+namespace TestClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Calling a GRPC Service");
+            Console.WriteLine("Hit enter to call the service....");
+            Console.ReadLine();
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Putintante" });
+
+            Console.WriteLine($"Got the response - {reply.Message}");
+
+            //Console.WriteLine("Hit enter to get directions..");
+            //Console.ReadLine();
+            //var clientRouting = new TurnByTurn.TurnByTurnClient(channel);
+
+            //var destination = new Destination { DestinationName = "Taco Bell" };
+
+            //var replyRoute = clientRouting.StartGuidance(destination);
+
+            //await foreach(var step in replyRoute.ResponseStream.ReadAllAsync())
+            //{
+            //    Console.WriteLine($"Turn {step.Direction} at {step.Road}");
+            //}
+
+            //Console.WriteLine("You have arrived. Enjoy your tacos");
+
+            Console.WriteLine("Hit enter to estimate your order pickup date");
+            Console.ReadLine();
+
+            var estimatorClient = new PickupEstimator.PickupEstimatorClient(channel);
+
+            var pickupRequest = new PickupRequest
+            {
+                For = "Jeff",
+
+            };
+            pickupRequest.Items.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+
+            var estimatorResponse = await estimatorClient.GetPickupTimeAsync(pickupRequest);
+            Console.WriteLine($"That'll be ready on {estimatorResponse.PickupTime.ToDateTime().ToShortDateString()}");
+        }
+    }
+}
